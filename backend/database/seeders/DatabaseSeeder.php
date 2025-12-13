4742da4beee65887e3868cf7304353a2a5c7742f
davidk <dkato81@gmail.com>
2025-12-13 14:26:03 +0300

    add initial data

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,18 +15,85 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $data = [
+            [
+                'name' => 'Alice Naki',
+                'email' => 'alice@gmail.com',
+                'password' => bcrypt('password'),
+                'balance' => 10000,
+            ],
+            [
+                'name' => 'David Kato',
+                'email' => 'david@gmail.com',
+                'password' => bcrypt('password'),
+                'balance' => 15000,
+            ],
+            [
+                'name' => 'Charlie Stevens',
+                'email' => 'charlie@gmail.com',
+                'password' => bcrypt('password'),
+                'balance' => 5000,
+            ]
+        ];
 
-        User::factory()->create([
-            'name' => 'David Kato',
-            'email' => 'davidkato@example.com',
-            'password' => bcrypt('password'),
-        ]);
+        $assets = [
+            [
+                [
+                    'symbol'=>'BTC',
+                    'amount'=>2.5,
+                ],
+                [
+                    'symbol'=>'ETH',
+                    'amount'=>10,
+                ],
+                [
+                    'symbol'=>'LTC',
+                    'amount'=>20,
+                ],
+                [
+                    'symbol'=>'XRP',
+                    'amount'=>500,
+                ]
+            ],
+            [
+                [
+                    'symbol'=>'ATC',
+                    'amount'=>1.0,
+                ],
+                [
+                    'symbol'=>'MTH',
+                    'amount'=>5,
+                ],
+                [
+                    'symbol'=>'LTC',
+                    'amount'=>15,
+                ],
+                [
+                    'symbol'=>'XRP',
+                    'amount'=>300,
+                ]
+            ],
+            [
+                [
+                    'symbol'=>'BTC',
+                    'amount'=>0.5,
+                ],
+                [
+                    'symbol'=>'MSH',
+                    'amount'=>2,
+                ],
+                [
+                    'symbol'=>'XRP',
+                    'amount'=>100,
+                ]
+            ]
+        ];
 
-        User::factory()->create([
-            'name' => 'John Musa',
-            'email' => 'johnmusa@example.com',
-            'password' => bcrypt('password'),
-        ]);
+        foreach ($data as $index => $userData) {
+            $user = User::factory()->create($userData);
+            foreach ($assets[$index] as $assetData) {
+                $user->assets()->create($assetData);
+            }
+        }
     }
 }
