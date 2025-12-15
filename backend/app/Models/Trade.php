012f03c41a3a7d7ed0544eff1c7762df56fea564
davidk <dkato81@gmail.com>
2025-12-15 23:02:01 +0300

    add trade model and migration

---
+++ backend/app/Models/Trade.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Trade extends Model
+{
+    protected $fillable = [
+        'buy_order_id',
+        'sell_order_id',
+        'price',
+        'amount',
+        'commission_rate',
+    ];
+
+    public function buyOrder()
+    {
+        return $this->belongsTo(Order::class, 'buy_order_id');
+    }
+
+    public function sellOrder()
+    {
+        return $this->belongsTo(Order::class, 'sell_order_id');
+    }
+}
